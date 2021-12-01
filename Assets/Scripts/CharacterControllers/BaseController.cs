346a72c72aaad753a2ddd25836024139980776c7
raranchik <raranchik007@gmail.com>
2021-12-02 01:15:06 +0700

    Init prj files

--- Assets/Scripts/CharacterControllers/BaseController.cs
+++ Assets/Scripts/CharacterControllers/BaseController.cs
@@ -8,14 +8,29 @@ namespace CharacterControllers
     {
         private float _speed = 3.5f;
         private float _jumpHeight = 5f;
-        private float _lookDir;
-        private bool _facingRight;
+        private float _dirX;
         private bool _onFloor;
         private bool _doJump;
 
         private Transform _transform;
         private Rigidbody2D _rb;
         private SpriteRenderer _sprRenderer;
+        private Animator _animator;
+
+        private enum AnimationStates
+        {
+            Idle = 0,
+            Run = 1,
+            Jump = 2,
+            JumpDouble = 3,
+            Falling = 4,
+            Landing = 5,
+            Hit = 6,
+            PushingForward = 7,
+            Attack1 = 8,
+            Attack2Sword = 9,
+            Death = 10
+        }
 
         // Start is called before the first frame update
         void Start()
@@ -23,24 +38,25 @@ namespace CharacterControllers
             _transform = GetComponent<Transform>();
             _sprRenderer = GetComponent<SpriteRenderer>();
             _rb = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
         }
 
         // Update is called once per frame
         void Update()
         {
-            _lookDir = Input.GetAxisRaw("Horizontal");
+            _dirX = Input.GetAxisRaw("Horizontal");
 
-            if (_lookDir < 0.0f && !_facingRight) Flip();
-            else if (_lookDir > 0.0f && _facingRight) Flip();
-
-            if (Input.GetButtonDown("Jump") && _onFloor)
+            if (((Input.GetButtonDown("Vertical") && Input.GetAxisRaw("Vertical") > 0) | Input.GetButtonDown("Jump")) && _onFloor)
                 _doJump = true;
+
+            UpdateAnimationState();
         }
 
         private void FixedUpdate()
         {
             Move();
-            if (_doJump)
+            
+            if (_doJump && _onFloor)
                 Jump();
         }
 
@@ -59,22 +75,34 @@ namespace CharacterControllers
         private void Move()
         {
             Vector2 velocity = _rb.velocity;
-            _rb.velocity = new Vector2(_lookDir * _speed, velocity.y);
+            _rb.velocity = new Vector2(_dirX * _speed, velocity.y);
         }
 
         private void Jump()
         {
-            if (_onFloor && _doJump)
-            {
-                _rb.AddForce(new Vector2(0, _jumpHeight), ForceMode2D.Impulse);
-                _doJump = false;
-            }
+            _rb.AddForce(new Vector2(0, _jumpHeight), ForceMode2D.Impulse);
+            _doJump = false;
         }
 
-        private void Flip()
+        private void UpdateAnimationState()
         {
-            _facingRight = !_facingRight;
-            _sprRenderer.flipX = _facingRight;
+            AnimationStates state = AnimationStates.Idle;
+
+            if (_dirX < 0)
+            {
+                _sprRenderer.flipX = true;
+                state = AnimationStates.Run;
+            }
+            else if (_dirX > 0)
+            {
+                _sprRenderer.flipX = false;
+                state = AnimationStates.Run;
+            }
+
+            if (_rb.velocity.y > .1f) state = AnimationStates.Jump;
+            else if (_rb.velocity.y < -.1f) state = AnimationStates.Falling;
+
+            _animator.SetInteger("AnimationState", (int) state);
         }
     }
 }
